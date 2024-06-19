c010ff16edf6b23e1db177d68a37f2b32d56c77c
Liu Dongmiao <liudongmiao@gmail.com>
2024-06-19 21:11:14 +0800

    wip: ci windows

--- testcase/run-test-udp.sh
+++ testcase/run-test-udp.sh
@@ -1,33 +1,53 @@
 #!/bin/bash
 
 set -e
-laddr=127.0.0.1
-lport=1081
-sport=8888
 
-if which ssserver; then
-    sslocal=sslocal
-    ssserver=ssserver
+if [ -f /usr/bin/python3 ]; then
+    python3=/usr/bin/python3
 else
-    curl -L https://github.com/shadowsocks/shadowsocks-rust/releases/download/v1.18.2/shadowsocks-v1.18.2.x86_64-unknown-linux-musl.tar.xz -O
-    tar xf shadowsocks-v1.18.2.x86_64-unknown-linux-musl.tar.xz
-    sslocal=./sslocal
-    ssserver=./ssserver
+    python3=python3
 fi
 
-if pgrep -f udp-echo-server.py > /dev/null; then
+if which ssserver >/dev/null 2>&1; then
+    sslocal=sslocal
+    ssserver=ssserver
+elif which apt-get >/dev/null 2>&1; then
+    curl -q -s -L https://github.com/shadowsocks/shadowsocks-rust/releases/download/v1.20.0/shadowsocks-v1.20.0.x86_64-unknown-linux-musl.tar.xz -O
+    tar xf shadowsocks-v1.20.0.x86_64-unknown-linux-musl.tar.xz
+    sslocal=./sslocal
+    ssserver=./ssserver
+else
+    curl -q -s -L https://github.com/shadowsocks/shadowsocks-rust/releases/download/v1.20.0/shadowsocks-v1.20.0.x86_64-pc-windows-msvc.zip -O
+    unzip shadowsocks-v1.20.0.x86_64-pc-windows-msvc.zip
+    sslocal=./sslocal.exe
+    ssserver=./ssserver.exe
+fi
+
+if which pgrep >/dev/null 2>&1 && pgrep -f udp-echo-server.py > /dev/null; then
     echo_server_pid=""
 else
-    /usr/bin/python3 ../testcase/udp-echo-server.py &
+    $python3 ../testcase/udp-echo-server.py &
     echo_server_pid=$!
 fi
 
 $ssserver -c ../testcase/sip003u-server.json &
 spid=$!
+sleep 1
 
 function cleanup() {
-    kill $echo_server_pid $lpid $spid
+    kill $echo_server_pid $lpid $lpid2 $spid
     rm -rf o.*
+}
+
+function check() {
+  for x in $(seq 1 3); do
+      if ! curl -q -s -v -x socks5h://127.0.0.1:$1 https://www.cloudflare.com/cdn-cgi/trace; then
+          exit 1
+      fi
+      if ! $python3 ../testcase/check-udp.py $1; then
+          exit 1
+      fi
+  done
 }
 
 trap cleanup SIGINT SIGTERM ERR EXIT
@@ -36,16 +56,10 @@ echo wss-proxy udp client - ss
 $sslocal -c ../testcase/sip003u-client-ss.json &
 lpid=$!
 sleep 1
-if ! /usr/bin/python3 ../testcase/check-udp.py; then
-  exit 1
-fi
-
-kill $lpid
+check 1081
 
 echo wss-proxy udp client - wss
 $sslocal -c ../testcase/sip003u-client-ws.json &
-lpid=$!
+lpid2=$!
 sleep 1
-if ! /usr/bin/python3 ../testcase/check-udp.py; then
-  exit 1
-fi
+check 1082
